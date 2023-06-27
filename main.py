000a59936c12b84674f6865cdde43ddcd7466e88
maya florez <florezmaya03@gmail.com>
2023-06-26 19:11:13 -0500

    adding conditional structure

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ clients = 'Pablo,Ricardo'
 
 def create_client(client_name):
     global clients
-    _add_comma()
-    clients += client_name
+    if client_name not in clients:
+        clients += ',' + client_name
+        _add_comma()
+    else:
+        print('client already is in the client\'s list')
 
 def list_clients():
     global clients
@@ -11,10 +14,28 @@ def list_clients():
 
 def _add_comma():
     global clients
-    if clients:
-        clients += ','
+    # if clients:
+    #     clients += ','
+
+def _print_welcome():
+    print('=' * 50)
+    print('Welcome to Platzi Ventas')
+    print('=' * 50)
+    print(f'What would you like to do today?')
+    print(f'[C]reate client')
+    print(f'[D]elete client')
+
 
 if __name__ == '__main__':
-    list_clients()
-    create_client('David')
-    list_clients()
+    _print_welcome()
+    
+    command = input()
+
+    if command == 'C':
+        client_name = input(f'What is the client name? ')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command')
