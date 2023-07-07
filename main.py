e484a6e9a05166c1faf78802c8e7319abece0ebe
maya florez <florezmaya03@gmail.com>
2023-07-07 13:33:18 -0500

    add while loop to the create client name

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 clients = 'Pablo,Ricardo'
 
 def create_client(client_name):
@@ -58,7 +60,18 @@ def _print_welcome():
 
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+    while not client_name:
+        client_name = input('What is the client name? ')
+
+        if client_name == 'exit':
+            client_name = None
+            break
+
+    if not client_name:
+        sys.exit()
+    
+    return client_name
 
 
 if __name__ == '__main__':
