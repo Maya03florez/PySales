e42ad60493622ed9c706139511a473743065d20c
maya florez <florezmaya03@gmail.com>
2023-07-08 11:16:44 -0500

    reestructre of the code

--- main.py
+++ main.py
@@ -1,44 +1,84 @@
 import sys
 
-clients = ['Pablo','Ricardo']
+clients = [
+    {
+        'name': 'pablo',
+        'company': 'google',
+        'email': 'pablo@google.com',
+        'position': 'software engineer',
+    },
+    {
+        'name': 'Ricardo',
+        'company': 'facebook',
+        'email': 'ricardo@facebook.com',
+        'position': 'data engineer'
+    }
+]
 
-def create_client(client_name):
+def create_client(client):
     global clients
-    if client_name not in clients:
-        clients.append(client_name)
+    if client not in clients:
+        clients.append(client)
     else:
         print('client already is in the client\'s list')
 
 
 def list_clients():
+    print('uid | name | company | email | position')
+    print('*' * 50)
+
     for idx, client in enumerate(clients):
-        print(f'{idx} : {client}')
+        print('{uid} | {name} | {company} | {email} | {position}'.format(
+            uid = idx,
+            name = client['name'],
+            company = client['company'],
+            email = client['email'],
+            position = client['position'],
+        ))
 
 
-def update_client(client_name,updated_name):
+def update_client(client_id,updated_client):
     global clients
-    if client_name in clients:
-        index = clients.index(client_name)
-        clients[index] = updated_name
+    if len(clients) - 1 >= client_id:
+        clients[client_id] = updated_client
     else:
-        print('Client is not in clients list')
+        print('Client is not in client\'s list.')
+        
 
 
-def delete_client(client_name):
+def delete_client(client_id):
     global clients
-    if client_name in clients:
-        clients.remove(client_name)
-    else:
-        print('Client is not in client list')
+    for idx,client in enumerate(clients):
+        if idx == client_id:
+            del clients[idx]
+            break
 
 
 def search_client(client_name):
     for client in clients:
-        if client != client_name:
+        if client['name'] != client_name:
             continue
         else:
             return True
 
+
+def _get_client_field(field_name,message = "waht is the client{}? "):
+    field = None
+
+    while not field:
+        field = input(f'What is the client {field_name}? ')
+    
+    return field
+
+
+def _get_client_from_user():
+    client = {
+        'name': _get_client_field('name'),
+        'company': _get_client_field('company'),
+        'email': _get_client_field('email'),
+        'position': _get_client_field('position'),
+    }
+    return client
 
 
 def _print_welcome():
@@ -53,21 +93,6 @@ def _print_welcome():
     print(f'[S]earch client')
 
 
-def _get_client_name():
-    client_name = None
-    while not client_name:
-        client_name = input('What is the client name? ')
-
-        if client_name == 'exit':
-            client_name = None
-            break
-
-    if not client_name:
-        sys.exit()
-    
-    return client_name
-
-
 if __name__ == '__main__':
     _print_welcome()
     
@@ -75,22 +100,23 @@ if __name__ == '__main__':
 
 
     if command == 'C':
-        client_name = _get_client_name()
-        create_client(client_name)
-        list_clients()
+       client = _get_client_from_user()
+
+       create_client(client)
+       list_clients()
     elif command == 'D':
-        client_name = _get_client_name()
-        delete_client(client_name)
+        client_id = int(_get_client_field('id'))
+        delete_client(client_id)
         list_clients()
     elif command == 'L':
         list_clients()
     elif command == 'U':
-        client_name = _get_client_name()
-        updated_name = input('What is the updated client name ')
-        update_client(client_name,updated_name)
+        client_id = int(_get_client_field('id'))
+        updated_client = _get_client_from_user()
+        update_client(client_id,updated_client)
         list_clients()
     elif command == 'S':
-        client_name = _get_client_name()
+        client_name = _get_client_field('name')
         found = search_client(client_name)
         if found:
             print(f'The client is in the client\'s list')
